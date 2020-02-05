15e2cbd5a8e505a569db11ae360bc10cc048b72b
ahorinka <ahorinka@infinitrac.com>
2020-02-05 15:31:22 +0100

    init files

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("I need an argument!");
+            }
+            
+            // get File
+            var fileInfo = new FileInfo(args[0]);
+            if (!fileInfo.Exists)
+            {
+                Console.WriteLine("No such file found");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
